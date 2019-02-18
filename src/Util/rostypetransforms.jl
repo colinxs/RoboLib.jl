f8b994339831ba70f15667253e7e152892ec22ac
Colin Summers <me@colinxsummers.com>
2019-02-17 20:16:50 -0800

    add ros type transforms

---
+++ src/Util/rostypetransforms.jl
@@ -0,0 +1,14 @@
+using JuliaDB
+using StaticArrays
+
+# TODO: put this + typegen in @requires statement so we don't always have to generate types
+
+# for Vector3, Point, etc...
+vec_xyz(x) = SVector(x.x, x.y, x.z)
+
+# for Quaternion
+vec_xyzw(x) = SVector(x.x, x.y, x.z, x.w)
+
+## for PoseWithCovariance, TwistWithCovaraince, etc...
+
+
